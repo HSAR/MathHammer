187bfdd2efc4db3cfa28f35deced27c99f80372b
Sam Chen <research.hyperspace@gmail.com>
2021-07-02 12:24:43 +0100

    Fixed aggressor weapon profile and tidied dice string parser.

--- src/main/kotlin/io/hsar/mathhammer/statistics/DiceStringParser.kt
+++ src/main/kotlin/io/hsar/mathhammer/statistics/DiceStringParser.kt
@@ -3,16 +3,18 @@ package io.hsar.mathhammer.statistics
 object DiceStringParser {
     fun expectedValue(diceString: String): Double {
         return diceString
-            .split("d") // remove dice character
-            .let { (numberOfDice, diceValue) ->
-                val numDice = if (numberOfDice.isEmpty()) {
-                    1
-                } else {
-                    numberOfDice.toInt()
+                .split("d") // remove dice character
+                .let { (numberOfDice, diceValue) ->
+                    if (numberOfDice.isEmpty()) {
+                        1
+                    } else {
+                        numberOfDice.toInt()
+                    }
+                            .let { numDice ->
+                                numDice * diceValue
+                                        .toInt() // this is the dice number, e.g. 6 for d6
+                                        .plus(1).div(2.0) // gives us the expected value
+                            }
                 }
-                numDice * diceValue
-                    .toInt() // this is the dice number, e.g. 6 for d6
-                    .plus(1).div(2.0) // gives us the expected value
-            }
     }
 }
